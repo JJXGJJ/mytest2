4a7c2c09b23824d882ef1f292fec54ede83076df
jjx <1107610438@qq.com>
2020-11-04 19:00:35 +0800

    第一次提交项目

---
+++ idea_git/src/com/jjx/git/Test.java
@@ -0,0 +1,13 @@
+package com.jjx.git;
+
+/**
+ * @Author JJ
+ * @Date 2020/11/4 18:56
+ * @Version 1.0
+ * @Describe
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
